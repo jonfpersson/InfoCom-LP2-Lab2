234b3c73568cab3f9bd895275a74b4d74a11bef8
j0nfp <jonfpersson@gmail.com>
2021-11-24 22:36:44 +0100

    test

--- webserver/database.py
+++ webserver/database.py
@@ -2,13 +2,15 @@ from flask import Flask, request
 from flask_cors import CORS
 import redis
 
+
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsa5'
 
 # change this to connect to your redis server
 # ===============================================
-redis_server = redis.Redis("YOUR_SERVER")
+redis_server = redis.Redis(host='127.0.0.1', port=6379)
+
 # ===============================================
 
 redis_server.set('longitude', 13.21008)
@@ -20,8 +22,17 @@ redis_server.set('latitude', 55.71106)
 # 3. write the updated data to the database
 # ===============================================
 def moveDrone(d_long, d_la):
-    pass
+    longitude = float(redis_server.get('longitude'))
+    latitude = float(redis_server.get('latitude'))
+
+    longitude += d_long
+    latitude += d_la
+
+    redis_server.set('longitude', longitude)
+    redis_server.set('latitude', latitude)
+
 # ===============================================
+
 
 @app.route('/drone', methods=['POST'])
 def drone():
